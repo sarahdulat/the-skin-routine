166c71191d4dd8898663006b68b4edc8a00124c6
Sarah Dulat <sarah.dulat@gmail.com>
2022-05-20 22:05:38 -0500

    adding glyphs

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg">
-    <a class="navbar-brand">The Skin Routine</a>
+    <a class="navbar-brand">The Skin Routine <span class="glyph">🩸</span></a>
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/blog">Blog</router-link>
