2eef5ee519f77da429211b9690f0389dae787da7
Sarah Dulat <sarah.dulat@gmail.com>
2022-05-20 21:27:16 -0500

    adding new files

---
+++ src/components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand">The Skin Routine</a>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/blog">Blog</router-link>
+  </nav>
+</template>
