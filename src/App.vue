d138be7ac0b93a399bf1d8af75acc3b8f99cc519
Sarah Dulat <sarah.dulat@gmail.com>
2022-05-21 21:45:25 -0500

    adding socials

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,17 @@
     <div class="position-absolute top-50 start-50 translate-middle">
       <h1>The Skin Routine<span class="glyph">🩸</span></h1>
       <h3>Coming Soon</h3>
+      <div class="social">
+        <a href="http://www.instagram.com/the_skinroutine" target="_blank">
+          <i class="bi bi-instagram fs-4" alt="instagram"></i>
+        </a>
+        <a href="http://www.twitter.com/the_skinroutine" target="_blank">
+          <i class="bi bi-twitter fs-4 ms-3" alt="twitter"></i>
+        </a>
+        <a href="http://www.pinterest.com/the_skinroutine" target="_blank">
+          <i class="bi bi-pinterest fs-4 ms-3" alt="pinterest"></i>
+        </a>
+      </div>
     </div>
   </div>
 </template>
