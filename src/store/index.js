a372ded1f06dc6bb5fd31bf2f722077ae778bcb5
Sarah Dulat <sarah.dulat@gmail.com>
2022-11-24 15:43:26 +0000

    cleaning up store

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,11 @@
 import { createStore } from "vuex";
-import posts from "./posts";
-import db from "../db";
 
 export default createStore({
   state: {
-    posts: posts
   },
   getters: {
   },
   mutations: {
-    setPosts(state) {
-      db.read()
-      db.data.posts = state.posts
-    }
   },
   actions: {
   },
